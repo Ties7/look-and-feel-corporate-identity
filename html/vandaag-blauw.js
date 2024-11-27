a37c4057133a2ecbe0203e0ea3c4e9040e7cc754
Ties7 <97968332+Ties7@users.noreply.github.com>
2024-11-27 18:19:32 +0100

    fix #18

--- html/vandaag-blauw.js
+++ html/vandaag-blauw.js
@@ -1,10 +1,21 @@
-const mybutton = document.querySelector("#test");
-const dom = document.body;
+const menu = document.querySelector(".menu");
+const menuButton = document.querySelector("#test");
 
-console.log(document.querySelector("#test"));
+// Open menu bij klikken van handle
+menuButton.addEventListener("click", function() {
+    menu.classList.toggle("open");
+});
 
-document.querySelector("#test").addEventListener("click", function() {
-    console.log(" tetsbenejd");
-    document.querySelector('.menu').classList.toggle("open");
+// Sluit menu bij klikken buiten menu
+document.addEventListener("click", function(event) {
+    // target = het geklikte element
+    const target = event.target;
 
+    // Sluit het menu als (beide moeten waar zijn):
+    // 1. Het element GEEN onderdeel is van het menu
+    // 2. Het element NIET de menu button is
+    if(!target.closest(".menu") && target !== menuButton) {
+        // Sluit het menu door de 'open' class weg te halen
+        menu.classList.remove("open");
+    }
 });
